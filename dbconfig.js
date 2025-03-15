402759feca01e3368ad947a7f0943fe79c5a0f74
Shyamal <info@bigsss.com>
2025-03-15 18:39:24 +0530

    Changed Database Port

--- dbconfig.js
+++ dbconfig.js
@@ -3,7 +3,7 @@ const { prototype } = require("jsonwebtoken/lib/JsonWebTokenError");
 const config = {
   user: "trade24x7_user", // Database username
   password: "Trd24*7aI", // Database password
-  server: "103.69.196.96", // Server IP address
+  server: "103.71.99.105", // Server IP address
   database: "trade24x7", // Database name
   options: {
     encrypt: false, // Disable encryption
