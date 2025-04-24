ad53bbafeeadba62eca5ed7516b9b80b9f742f71
Shyamal <info@bigsss.com>
2025-04-24 16:31:24 +0530

    Another Change

--- controllers/user.js
+++ controllers/user.js
@@ -292,9 +292,9 @@ exports.getCirculatingSupply = async (req, res, next) => {
     // const lockedTokens4 = await getWalletBalance(
     //   "0xAFA434D7970F62eb6f0BA4DaB81754B4C2655AAE"
     // );
-    // const lockedTokens5 = await getWalletBalance(
-    //   "0xEDDf191e5581C7aFd9B634B48C1c4a2cAbAeF8D4"
-    // );
+    const lockedTokens5 = await getWalletBalance(
+      "0xEDDf191e5581C7aFd9B634B48C1c4a2cAbAeF8D4"
+    );
 
     // let lockedTokens = 0;
     // for (let wallet of LOCKED_WALLETS) {
@@ -308,7 +308,8 @@ exports.getCirculatingSupply = async (req, res, next) => {
       burnedTokens -
       lockedTokens1 -
       lockedTokens2 -
-      lockedTokens3;
+      lockedTokens3 -
+      lockedTokens5;
     res.json({ circulating_supply: circulatingSupply });
   } catch (error) {
     res.status(500).json({ error: "Error fetching data" });
